6cfe097d6447a24fc12476906d8997a6585eba3e
isher2k1 <ikekches@gmail.com>
2026-01-20 15:42:29 +0200

    Add class MyLinkedList.java

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,45 +3,161 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
     @Override
     public void add(T value) {
+        Node<T> newNode = new Node<>(tail, value, null);
+
+        if (tail == null) {
+            head = newNode;
+        } else {
+            tail.next = newNode;
+        }
+
+        tail = newNode;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Invalid index!");
+        }
+
+        if (index == size) {
+            add(value);
+        } else if (index == 0) {
+            Node<T> newNode = new Node<>(null, value, head);
+            head.prev = newNode;
+            head = newNode;
+            size++;
+        } else {
+            Node<T> nodeAtIndex = node(index);
+            Node<T> newNode = new Node<>(nodeAtIndex.prev, value, nodeAtIndex);
+            nodeAtIndex.prev.next = newNode;
+            nodeAtIndex.prev = newNode;
+            size++;
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
+        if (list == null) {
+            throw new NullPointerException("List is null!");
+        }
+
+        for (T e : list) {
+            add(e);
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        return node(index).item;
     }
 
     @Override
     public T set(T value, int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index!");
+        }
+
+        Node<T> nodeAtIndex = node(index);
+        T oldItem = nodeAtIndex.item;
+        nodeAtIndex.item = value;
+        return oldItem;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        return unlink(node(index));
     }
 
     @Override
     public boolean remove(T object) {
+        Node<T> searchNode = head;
+
+        while (searchNode != null) {
+            if (object == null ? searchNode.item == null
+                    : object.equals(searchNode.item)) {
+                unlink(searchNode);
+                return true;
+            } else {
+                searchNode = searchNode.next;
+            }
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    private Node<T> node(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index!");
+        }
+
+        Node<T> node;
+
+        if (index < size / 2) {
+            node = head;
+
+            for (int i = 0; i < index; i++) {
+                node = node.next;
+            }
+
+            return node;
+        } else {
+            node = tail;
+
+            for (int i = size - 1; i > index; i--) {
+                node = node.prev;
+            }
+
+            return node;
+        }
+    }
+
+    private T unlink(Node<T> node) {
+        T item = node.item;
+        Node<T> prev = node.prev;
+        Node<T> next = node.next;
+
+        if (prev == null) {
+            head = next;
+        } else {
+            prev.next = next;
+        }
+
+        if (next == null) {
+            tail = prev;
+        } else {
+            next.prev = prev;
+        }
+
+        size--;
+        return item;
     }
 }
